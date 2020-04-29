80d43dab8b165cd46a9186c0979fa16294a5b88d
Wataru Ono <wtr0414@gmail.com>
2020-04-30 08:00:23 +0900

    [Add] 状態変更機能と削除機能の追加

---
+++ main.js
@@ -0,0 +1,56 @@
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    todos: []
+  },
+  methods: {
+    // ToDo 追加の処理
+    doAdd: function (event, value) {
+      var comment = this.$refs.comment
+      if (!comment.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+      comment.value = ''
+    },
+    doChangeState: function (item) {
+      item.state = item.state ? 0 : 1
+    },
+    doRemove: function (item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
+    }
+  },
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
+  created() {
+    this.todos = todoStorage.fetch()
+  }
+})
