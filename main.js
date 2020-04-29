84865ea1277674e692ca9dc61d459b52934073fe
Wataru Ono <wtr0414@gmail.com>
2020-04-30 08:12:15 +0900

    [Update] 状態の表記を[0, 1]から[作業中, 完了]に変更

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const app = new Vue({
   data: {
     todos: [],
     options: [
-      { value: -1, labe: 'すべて' },
+      { value: -1, label: 'すべて' },
       { value: 0, label: '作業中' },
       { value: 1, label: '完了' }
     ],
@@ -49,6 +49,11 @@ const app = new Vue({
     }
   },
   computed: {
+    labels() {
+      return this.options.reduce(function (a, b) {
+        return Object.assign(a, { [b.value]: b.label })
+      }, {})
+    },
     computedTodos: function () {
       return this.todos.filter(function (el) {
         return this.current < 0 ? true : this.current === el.state
